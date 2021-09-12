d2c38231e1d345410d66739109949ba96da6d41f
stan svec <stan.x.svec@gmail.com>
2021-09-12 15:32:54 +0100

    view flexible

--- tarocapp/cmd/ps.py
+++ tarocapp/cmd/ps.py
@@ -14,15 +14,15 @@ def run(args):
 
 
 async def run_ps(args):
-    host_to_task = taroc.ps('ciserver')
-    jobs_view = JobsView(hosts_count=len(host_to_task), table_title='Running Jobs', columns=COLUMNS)
+    host_to_task = taroc.ps('ciserver', 'prdextdata')
+    jobs_view = JobsView(hosts_count=len(host_to_task), columns=COLUMNS)
     with Live(jobs_view) as live_view:
         for next_done in asyncio.as_completed(host_to_task.values()):
             host_instances = await next_done
-            rows = [_instance_to_row(inst) for inst in host_instances[1]]
+            rows = [_instance_to_row(host_instances[0], inst) for inst in host_instances[1]]
             jobs_view.add_host_rows(host_instances[0], rows)
             live_view.refresh()
 
 
-def _instance_to_row(inst):
-    return ['host1', inst['job_id'], 'instance1', '2012-09-02', '2012-09-02', '2h', 'RUNNING', '', 'Bla bla']
+def _instance_to_row(host, inst):
+    return [host, inst['job_id'], 'instance1', '2012-09-02', '2012-09-02', '2h', 'RUNNING', '', 'Bla bla']
--- tarocapp/view.py
+++ tarocapp/view.py
@@ -4,11 +4,12 @@ from rich.console import Group
 from rich.padding import Padding
 from rich.panel import Panel
 from rich.progress import Progress, BarColumn, TimeRemainingColumn
+from rich.spinner import Spinner
 from rich.table import Table
 
 
-def _init_table(table_title, columns):
-    table = Table(title=table_title, box=box.SIMPLE)
+def _init_table(columns):
+    table = Table(box=box.SIMPLE)
     for column in columns:
         table.add_column(column)
     table.columns[-1].justify = 'full'
@@ -17,18 +18,30 @@ def _init_table(table_title, columns):
 
 class JobsView:
 
-    def __init__(self, *, hosts_count, table_title, columns):
-        self.status_panel = StatusPanel(hosts_count)
-        self.table = _init_table(table_title, columns)
-        self.view = Group(self.status_panel, self.table)
+    def __init__(self, *, hosts_count, columns):
+        self._status_panel = StatusPanel(hosts_count)
+        self._table = _init_table(columns)
+        self._spinner = Spinner('simpleDotsScrolling', "[bold green]Fetching jobs...")
+        self._hosts_count = hosts_count
+        self._hosts_completed = 0
 
     def add_host_rows(self, host, rows):
-        self.status_panel.completed()
+        self._hosts_completed += 1
+        self._status_panel.completed()
         for row in rows:
-            self.table.add_row(*row)
+            self._table.add_row(*row)
+
+    def is_completed(self):
+        return self._hosts_completed == self._hosts_count
 
     def __rich__(self):
-        return self.view
+        renders = [self._status_panel]
+        if len(self._table.rows) > 0:
+            renders.append(self._table)
+        if not self.is_completed():
+            renders.append(self._spinner)
+
+        return Group(*renders)
 
 
 class StatusPanel:
