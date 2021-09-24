0db27ec73505243c58730f0dded3928f9624f9b2
stan svec <stan.x.svec@gmail.com>
2021-09-24 15:40:22 +0100

    ssh connection timeout

--- taroc/sshclient.py
+++ taroc/sshclient.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 from dataclasses import dataclass
 from typing import TypeVar, Callable, Generic, List, Awaitable
@@ -11,9 +12,7 @@ from taroc.job import JobInstance
 @dataclass(frozen=True)
 class HostInfo:
     """
-    Host information available before communicating with the host.
-
-    Note: This must be moved to more generic module when more network protocols are supported.
+    Information needed for communication with a host.
     """
 
     host: str
@@ -31,6 +30,7 @@ class Response(Generic[T]):
     """
 
     host: str
+    success: bool
     body_obj: T
 
 
@@ -40,11 +40,18 @@ def create_tasks(command: str, resp_deser: Callable[[str], T], *hosts: HostInfo)
 
 
 async def _exec(host_info: HostInfo, command: str, resp_deser: Callable[[str], T]) -> Response[T]:
-    async with asyncssh.connect(host_info.host, login_timeout=cfg.ssh_con_timeout) as conn:
+    conn_task = asyncssh.connect(host_info.host, login_timeout=cfg.ssh_con_timeout)
+    try:
+        conn = await asyncio.wait_for(conn_task, cfg.ssh_con_timeout)
+    except asyncio.TimeoutError:
+        return Response(host_info.host, False, None)
+
+    async with conn:
         result = await conn.run(f"python3 -m taroapp {command}", check=True, timeout=cfg.ssh_run_timeout)
-        result_str = result.stdout
-        resp_body: T = resp_deser(result_str)
-        return Response(host_info.host, resp_body)
+
+    result_str = result.stdout
+    resp_body: T = resp_deser(result_str)
+    return Response(host_info.host, True, resp_body)
 
 
 def ps(*hosts: HostInfo) -> dict[HostInfo, Awaitable[Response[List[JobInstance]]]]:
