4b48d1da1066a5de54cf6a01bf4a9a40b42ffe72
Stan Svec <stan@taggstar.com>
2021-10-06 20:28:33 +0700

    test job model error update

--- test/test_model.py
+++ test/test_model.py
@@ -3,11 +3,12 @@ from tarocapp.model import JobInstancesModel
 from test.util import *
 
 
-def test_observing():
+def test_add_jobs_update():
     # Given
     observed = 0
 
     def model_update(model, event):
+        # Then:
         assert sut == model
         assert JobInstances([J1_2, J2_1]) == event.new_instances
         nonlocal observed
@@ -20,5 +21,22 @@ def test_observing():
 
     # When:
     sut.add_host_jobs('any', [J1_2, J2_1])
+
+    # Then:
+    assert observed == 1
+
+
+def test_add_error_update():
+    observed = 0
+
+    def model_update(_, event):
+        assert len(event.new_instances) == 0
+        nonlocal observed
+        observed += 1
+
+    sut = JobInstancesModel(1)
+    sut.observers.append(model_update)
+
+    sut.add_host_error('any', None)
 
     assert observed == 1
